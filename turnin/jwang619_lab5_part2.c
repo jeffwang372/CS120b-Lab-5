6f73199fa0a4904881c24694a9ce34aafee296a0
jeffwang372 <jeffwang372@gmail.com>
2021-04-25 22:36:44 -0700

    Completed part 3

--- turnin/jwang619_lab5_part2.c
+++ turnin/jwang619_lab5_part2.c
@@ -1,6 +1,6 @@
 /* Author: Jeffrey Wang jwang619@ucr.edu
  * Lab Section: 22
- * Assignment: Lab #5  Exercise #1
+ * Assignment: Lab #5  Exercise #2
  * Exercise Description: [optional - include for your own benefit]
  *
  * I acknowledge all content contained herein, excluding template or example
